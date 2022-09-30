bcc5cd5a0f011e4e8371197984dfd8c487c52429
lolaeurich <lola.tatti@gmail.com>
2022-09-30 15:10:30 -0300

    done

--- crud.sql
+++ crud.sql
@@ -4,31 +4,75 @@
 -- Criação
 
 -- 1)
-
+INSERT INTO
+	clientes(nome, lealdade)
+    VALUES
+    ('Georgia', 0);
 
 
 -- 2)
-
+INSERT INTO
+	pedidos(status, cliente_id)
+    VALUES
+    	('Recebido', 6);  
 
 
 -- 3)
-
+INSERT INTO 
+    produtos_pedidos(pedido_id, produto_id)
+VALUES
+	(6, 1),
+    (6, 2),
+    (6, 6),
+    (6, 8),
+    (6, 8);
 
 
 -- Leitura
 
 -- 1)
+SELECT 
+	clientes.*,
+    pedidos.*,
+    produtos.*
+FROM
+	clientes 
+JOIN pedidos ON pedidos.cliente_id = clientes.id 
+JOIN produtos_pedidos ON produtos_pedidos.pedido_id = pedidos.id
+JOIN produtos ON produtos.id = produtos_pedidos.produto_id
 
+WHERE
+	clientes.id = 6;
 
 
 -- Atualização
 
 -- 1)
+SELECT
+	SUM(produtos.pts_de_lealdade)
+FROM
+	clientes 
+JOIN pedidos ON pedidos.cliente_id = clientes.id 
+JOIN produtos_pedidos ON produtos_pedidos.pedido_id = pedidos.id
+JOIN produtos ON produtos.id = produtos_pedidos.produto_id
+WHERE
+	clientes.id = 6;
 
+
+UPDATE
+	clientes
+SET
+	lealdade = 48
+WHERE 
+	clientes.id = 6;    
 
 
 -- Deleção
 
 -- 1)
+DELETE FROM 
+	clientes
+WHERE 
+	id = 4;
 
 
--- selects.sql
+++ selects.sql
@@ -2,11 +2,47 @@
 -- Seleções de dados
 
 -- 1)
+SELECT 
+	pedidos.*, produtos.*
+FROM
+  	pedidos 
+INNER JOIN produtos_pedidos ON pedidos.id = produtos_pedidos.pedido_id
+INNER JOIN produtos ON produtos.id = produtos_pedidos.produto_id;
 
 -- 2)
+SELECT 
+	produtos_pedidos.pedido_id
+FROM
+  	produtos_pedidos 
+WHERE 
+	produtos_pedidos.produto_id = 6;
 
 -- 3)
+SELECT
+  clientes.nome as gostam_de_fritas
+FROM
+  clientes 
+INNER JOIN pedidos ON clientes.id = pedidos.cliente_id
+INNER JOIN produtos_pedidos ON pedidos.id = produtos_pedidos.pedido_id
+WHERE
+  produtos_pedidos.produto_id = 6;
 
 -- 4)
+SELECT
+  SUM(produtos.preço)
+FROM
+  pedidos 
+INNER JOIN produtos_pedidos ON pedidos.id = produtos_pedidos.pedido_id
+INNER JOIN produtos ON produtos.id = produtos_pedidos.produto_id
+INNER JOIN clientes ON clientes.id = pedidos.cliente_id
+WHERE
+	clientes.nome LIKE 'Laura';
 
 -- 5)
+SELECT
+  produtos.nome, COUNT(produtos_pedidos.produto_id)
+FROM
+  produtos
+INNER JOIN produtos_pedidos ON produtos.id = produtos_pedidos.produto_id
+GROUP BY
+  produtos.nome;
